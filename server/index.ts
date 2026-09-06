649102e98fed46700d890ce59f118459ba4c48e1
Aboubakary Traore <abou@dhcp-10-84-128-115.wireless.buffalo.edu>
2026-09-05 23:58:56 -0400

    Fixed SDK issues

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import { GoogleGenAI, Type} from "@google/genai";
+import { GoogleGenAI,}  from "@google/genai";
 import "dotenv/config";
 //For reading from terminal.
 const rl = readline.createInterface({ input, output });
@@ -8,6 +8,9 @@ const rl = readline.createInterface({ input, output });
 const ai = new GoogleGenAI({
  apiKey : process.env.GEMINI_API_KEY
 });
+
+
+
 //INTERFACES---------------
 //Content
 interface Content{
@@ -21,52 +24,41 @@ interface Message{
 }
 
 //GEMINI--------------------
-interface Thought{
-  signature : string,
-  type : string
-}
-const Step : typeof Step;
-//_________
-const history : (Message | Thought | Step)[] = [];
+
+const history: any[] = [];
+
 //------------------------------
 //Array of Messages
 
-while(true){
-const prompt : string = await rl.question('Please type in a prompt. ');
-if (prompt == "Bye" || prompt == "bye" || prompt == "Quit" || prompt == "quit"){
-  break;
-}
 
-const con : Content = {
-  type:"text",
-  text: prompt
-}
+while (true) {
+  const prompt: string = await rl.question('Please type in a prompt. ');
 
-let message : Message = {
-  type:"user_input",
-  content: [con]
-}
+  if (prompt == "Bye" || prompt == "bye" || prompt == "Quit" || prompt == "quit") {
+    break;
+  }
 
-history.push(message);
+  const con: Content = {
+    type: "text",
+    text: prompt
+  };
 
-const interaction = await ai.interactions.create({
-  model: "gemini-3.5-flash-lite",
-  input: prompt, 
-  store : false
-});
+  const message: Message = {
+    type: "user_input",
+    content: [con]
+  };
 
-interaction.steps.forEach((step)=> history.push(step))
+  history.push(message);
 
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.5-flash-lite",
+    input: history,
+    store: false
+  });
 
+  interaction.steps.forEach((step) => history.push(step));
 
-console.log(interaction.output_text);
-//Debugging
-/*
-console.log(interaction.steps);
-let check = interaction.steps[1];
-if(check.type === "model_output")
-console.log(check.content)
-*/
+  console.log(interaction.output_text);
 }
  rl.close();
 
